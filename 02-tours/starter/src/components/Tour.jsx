ee665d9dc719d75756dbb30f82718932c3afa962
Emile0001 <boteseh@gmail.com>
2024-10-03 20:11:46 +0200

    read more toggle

--- 02-tours/starter/src/components/Tour.jsx
+++ 02-tours/starter/src/components/Tour.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 export const Tour = ({ id, name, image, info, price, removeTour }) => {
+    const [readMore, setReadMore] = useState(false);
     return (
         <article className="single-tour">
             <img src={image} alt={name} className="img" />
@@ -6,8 +9,14 @@ export const Tour = ({ id, name, image, info, price, removeTour }) => {
             <div className="tour-info">
                 <h5>{name}</h5>
                 <p>
-                    {info}
-                    <button className="info-btn">read more</button>
+                    {readMore ? info : `${info.substring(0, 200)}...`}
+                    <button
+                        type="button"
+                        className="info-btn"
+                        onClick={() => setReadMore(!readMore)}
+                    >
+                        {readMore ? "show less" : "read more"}
+                    </button>
                 </p>
                 <button
                     type="button"
