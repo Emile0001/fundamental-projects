5f8b28b1fc92c095b35d798a051f87809db6c2a3
Emile0001 <boteseh@gmail.com>
2024-10-06 18:13:05 +0200

    refactor

--- 03-reviews/starter/src/App.jsx
+++ 03-reviews/starter/src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from "react";
-import { Reviews } from "./components/Reviews";
 
 const App = () => {
-    const [reviews, setReviews] = useState();
     return (
         <main>
             <h1>Reviews</h1>
-            <Reviews />
         </main>
     );
 };
