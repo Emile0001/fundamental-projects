93f0de1c6aab55adf34880daf4fdac73e2c83a68
Emile0001 <boteseh@gmail.com>
2024-10-06 18:20:02 +0200

    display person

--- 03-reviews/starter/src/App.jsx
+++ 03-reviews/starter/src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
-
+import people from "./data";
 const App = () => {
+    const [index, setIndex] = useState(0);
+    const { id, name, job, image, text } = people[index];
+
     return (
         <main>
-            <h1>Reviews</h1>
+            <section>
+                <img src={image} alt={name} />
+                <h4>{name}</h4>
+                <p>{job}</p>
+                <p>{text}</p>
+            </section>
         </main>
     );
 };
